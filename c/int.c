4e0012876e1c39c89c27f5553ed92f5eee8f3979
ajay babu <ajaymehra9508@gmail.com>
2024-07-29 16:26:28 +0530

    output

---
+++ c/int.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main(){
+    int age = 232;
+    int name  = 'w';
+    float distanc =235.5769;
+    printf("your name is %c \n",name);
+    printf("your age is %d \n",age);
+    printf("distance of your home is %f \n",distanc);
+}
