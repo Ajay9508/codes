2f57cedb88d07d0182bd4c5cd3261dc629425c37
ajay babu <ajaymehra9508@gmail.com>
2024-09-12 13:23:54 +0530

    without__Responsitve

---
+++ c/add.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main(){
+    int a,b,c;
+    printf("Enter the value a and b");
+    scanf("%d",&a);
+    scanf("%d",&b);
+    printf("add both value:%d",a+b);
+    printf("substract both value:%d",a-b);
+}
---
+++ c/second.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main()
+{
+ printf("hello ajay how are you./.......\n");
+ //kaise ho ajy 
+ printf("hello mr.developer");
+}
