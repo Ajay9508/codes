ddf9b647c32504765319fa98a867fbb2968187c6
Ajay kumar <121481541+Ajay9508@users.noreply.github.com>
2026-01-12 20:05:39 +0530

    Delete c directory

--- c/int.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-void main(){
-    int age = 232;
-    int name  = 'w';
-    float distanc =235.5769;
-    printf("your name is %c \n",name);
-    printf("your age is %d \n",age);
-    printf("distance of your home is %f \n",distanc);
-}
--- c/second.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-int main()
-{
- printf("hello ajay how are you./.......\n");
- //kaise ho ajy 
- printf("hello mr.developer");
-}
